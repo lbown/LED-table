9a2cbeacbbec33d9d765c4fa3ab1c6c533afc528
Logan Bown <lbown@trinity.edu>
2018-06-29 00:52:32 -0500

    Adding comments as I plan structure

--- pyled/pyled.py
+++ pyled/pyled.py
@@ -1,22 +1,44 @@
+#!/usr/bin/env python2
 import sys
 import settings
+import collections
 
 # TODO: import correct movie driver based on config setting
 
 def main(*args):
 	
 	# Specifies behavior to follow once pictureStack is exhausted
-	queueDoneAction = None # TODO: read queue done action from config
+	actionTimer = None # TODO: read queue done action from config
+	
+	# Strings specifying what actions are next. Loops by default
+	actionQueue = collections.deque([]) # TODO: this needs to be _bound_ to a user-maintained queue (both read and write required from client and table)
+	actionQueueUpdate = False
 
+	actionObject = getActionObject(actionQueue[0])
+	actionQueue.rotate(-1)
 
-	movieQueue = [] # TODO: read 
-	pictureQueue = []
+	# Action
+		# parent of Movie
+		# parent of Game -- don't terminate Game objects on timer, Game objects send a finished signal back to the loop
+		# parent of StateMachine
+		# parent of ResponsiveVisualization
+		###
+		# Has tick method - returns a picture
+		# __delete__ function needs to correctly relinquish all input. An option is to 
 
 	# Main loop: execute every 1/60 second
-		# Check input sources and update queueDoneAction
-		# If len(pictureStack) == 1
+
+								### Maintain action object ###
+
+		# Loop through Action queue based on timer maintained here, or signal recieved from Game object
+			# Request update from Action object, send response picture to output driver
+
+								### Maintain binding on actionQueue ###
+		# If actionQueueUpdate and len(actionQueue) > 0 
+			# actionObject.__delete__()
+			# actionObject = getActionObject(actionQueue[0])
+			# actionQueue.rotate(-1)
 
 
 if __name__ == '__main__':
 	main(*sys.argv[1:])
-	print setings.DEFAULT_MOVEIS
